f63430f28f304b4d07117e870b6c368a8a06a60e
unknown <rungrodks@hotmail.com>
2019-01-21 16:36:11 +0700

    https://github.com/rungrodkspeed/review_exercise1

---
+++ review_exercise1_5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    double height,base,area;
+    cout << "Enter height: ";
+    cin >> height;
+    cout << "Enter base: ";
+    cin >> base;
+    cout << "Area = " << 0.5*height*base;
+    return 0;
+}
